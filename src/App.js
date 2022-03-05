13f61b30c80cf8fb36e0b7bc9da54005eec44353
Terban Illia <terban.illja@gmail.com>
2022-03-05 12:10:46 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import './App.css';
 
 function App() {
     return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <ul>
+            <li>
+                <a href="#">Link1</a>
+                <a href="#">Link2</a>
+                <a href="#">Link3</a>
+                <a href="#">Link4</a>
+            </li>
+        </ul>
+    )
+}
+
+function Technologies() {
+    return (
         <ul>
             <li>html</li>
             <li>css</li>
             <li>js</li>
             <li>react</li>
         </ul>
-    );
+    )
 }
 
 export default App;

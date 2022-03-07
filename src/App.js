33a254e1aa40e7a3bfa7121101dab0a79bf89287
Terban Illia <terban.illja@gmail.com>
2022-03-07 15:16:09 +0200

    Routes, BrowserRouter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Route, Routes} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 function App() {
     return (
@@ -11,10 +15,15 @@ function App() {
             <main className='main'>
                 <Navbar/>
                 <div className='content'>
-                    <Profile/>
-                    {/*<Dialogs/>*/}
+                    <Routes>
+                        <Route path="profile" element={<Profile/>}/>
+                        <Route path="dialogs" element={<Dialogs/>}/>
+                        <Route path="news" element={<News/>}/>
+                        <Route path="music" element={<Music/>}/>
+                        <Route path="settings" element={<Settings/>}/>
+                        <Route path="*" element={<Profile/>}/>
+                    </Routes>
                 </div>
-
             </main>
         </div>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,28 @@
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
+
 
 function Navbar() {
     return (
-        <nav className={s.navbar}>
-            <ul className={s.navList}>
-                <li className={s.navListItem}>
-                    <a className={`${s.navListLink} ${s.active}`}>Profile</a>
-                </li>
-                <li className={s.navListItem}>
-                    <a className={s.navListLink}>Messages</a>
-                </li>
-                <li className={s.navListItem}>
-                    <a className={s.navListLink}>News</a>
-                </li>
-                <li className={s.navListItem}>
-                    <a className={s.navListLink}>Music</a>
-                </li>
-                <li className={s.navListItem}>
-                    <a className={s.navListLink}>Settings</a>
-                </li>
-            </ul>
-        </nav>
+            <nav className={s.navbar}>
+                <ul className={s.navList}>
+                    <li className={s.navListItem}>
+                        <NavLink to='/profile' className={s.navListLink}>Profile</NavLink>
+                    </li>
+                    <li className={s.navListItem}>
+                        <NavLink to='/dialogs' className={s.navListLink}>Messages</NavLink>
+                    </li>
+                    <li className={s.navListItem}>
+                        <NavLink to='/news' className={s.navListLink}>News</NavLink>
+                    </li>
+                    <li className={s.navListItem}>
+                        <NavLink to='/music' className={s.navListLink}>Music</NavLink>
+                    </li>
+                    <li className={s.navListItem}>
+                        <NavLink to='/settings' className={s.navListLink}>Settings</NavLink>
+                    </li>
+                </ul>
+            </nav>
     )
 }
 

a2993581873e95f895ea98d9d0f343a37a258619
Terban Illia <terban.illja@gmail.com>
2022-03-07 16:46:10 +0200

    NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,19 +7,19 @@ function Navbar() {
             <nav className={s.navbar}>
                 <ul className={s.navList}>
                     <li className={s.navListItem}>
-                        <NavLink to='/profile' className={s.navListLink}>Profile</NavLink>
+                        <NavLink to='/profile' className={s.navLink}>Profile</NavLink>
                     </li>
                     <li className={s.navListItem}>
-                        <NavLink to='/dialogs' className={s.navListLink}>Messages</NavLink>
+                        <NavLink to='/dialogs' className={s.navLink}>Messages</NavLink>
                     </li>
                     <li className={s.navListItem}>
-                        <NavLink to='/news' className={s.navListLink}>News</NavLink>
+                        <NavLink to='/news' className={s.navLink}>News</NavLink>
                     </li>
                     <li className={s.navListItem}>
-                        <NavLink to='/music' className={s.navListLink}>Music</NavLink>
+                        <NavLink to='/music' className={s.navLink}>Music</NavLink>
                     </li>
                     <li className={s.navListItem}>
-                        <NavLink to='/settings' className={s.navListLink}>Settings</NavLink>
+                        <NavLink to='/settings' className={s.navLink}>Settings</NavLink>
                     </li>
                 </ul>
             </nav>

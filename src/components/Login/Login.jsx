b9abc8895bdc12930249cd625f397547d51e18f2
Terban Illia <terban.illja@gmail.com>
2022-04-06 11:58:12 +0300

    Formik (Login)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,39 @@
+import s from "./Login.module.css"
+import {Formik, Form, Field} from 'formik';
+
+
 function Login() {
     return (
-        <h1>Login</h1>
+        <div className={s.wrapper}>
+            <Formik
+                initialValues={{login: '', password: '', rememberMe: false}}
+                onSubmit={(values, {setSubmitting}) => {
+                    setTimeout(() => {
+                        console.log(values)
+                        setSubmitting(false);
+                    }, 1000);
+                }}
+            >
+                {({isSubmitting}) => (
+                    <Form className={s.form}>
+                        <div className={s.row}>
+                            <Field type="text" name="login" className={s.login} id="login"/>
+                            <label className={s.label} htmlFor="login">Login</label>
+                        </div>
+                        <div className={s.row}>
+                            <Field type="password" name="password" className={s.password} id="password"/>
+                            <label className={s.label} htmlFor="password">Password</label>
+                        </div>
+                        <div className={s.checkbox}>
+                            <Field type="checkbox" name="rememberMe" placeholder="" className={s.rememberMe}
+                                   id="rememberMe"/>
+                            <label className={s.label} htmlFor="rememberMe">Remember me</label>
+                        </div>
+                        <button type="submit" disabled={isSubmitting} className={s.button}>Login</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     )
 }
 
